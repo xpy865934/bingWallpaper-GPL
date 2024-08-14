ac1b3798c6caeb653f92b4c38a69cbe9dab6d1fb
xpy <86934097@qq.com>
2024-08-14 10:37:47 +0800

    init

---
+++ src/main/java/com/xpy/bingwallpapergpl/BingWallpaperGplApplication.java
@@ -0,0 +1,13 @@
+package com.xpy.bingwallpapergpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingWallpaperGplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BingWallpaperGplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xpy/bingwallpapergpl/BingWallpaperGplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpy.bingwallpapergpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingWallpaperGplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
